00c7fbe5a47978c901a584af5c2a40966e08fabd
ss <s@s.com>
2020-11-08 18:30:16 +0100

    Done: added state schema descriptions (joi)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import produce from "immer";
+import Joi from "joi";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -6,14 +8,58 @@ import "./App.css";
 // look into immutable ways of adding elements to arrays
 // onClick -> e.stopPropagation -> onMouseDown - doesn't work?
 // incorporate immer into the application
+// incorporate joi schema description language library into the app (and document the state object)
 // Implement cables with regular (rotated) divs
+// inlet/outlet pin display name when some key is pressed
+// Display an error if there two nodes with the same if
+// Display an error if there are two outlet pins with the same name within a node
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {
-			value: "Test",
+		const stateSchema = Joi.object({
+			value: Joi.string(),
+			nodePinnedId: Joi.string().allow(null).required(),
+			nodeInspectedId: Joi.string().allow(null).required(),
+			initialNodeDragOffset: Joi.object({
+				x: Joi.number().required(),
+				y: Joi.number().required(),
+			}).required(),
+			nodes: Joi.array()
+				.items(
+					Joi.object({
+						id: Joi.string().required(),
+						nodePos: Joi.object({
+							x: Joi.number().required(),
+							y: Joi.number().required(),
+						}),
+						outletPinNames: Joi.array()
+							.items(Joi.string())
+							.required(),
+					})
+				)
+				.required(),
+			startNode: Joi.object({
+				position: Joi.object({
+					x: Joi.number().required(),
+					y: Joi.number().required(),
+				}).required(),
+			}).required(),
+			cableDrag: Joi.object({
+				isDragging: Joi.bool().required(),
+				initialPos: Joi.object({
+					x: Joi.number().required(),
+					y: Joi.number().required(),
+				}).required(),
+				endPos: Joi.object({
+					x: Joi.number().required(),
+					y: Joi.number().required(),
+				}).required(),
+			}).required(),
+		});
+
+		const initialState = {
 			nodePinnedId: null,
 			nodeInspectedId: null,
 			initialNodeDragOffset: {
@@ -27,6 +73,7 @@ class App extends React.Component {
 						x: 0,
 						y: 0,
 					},
+					outletPinNames: ["1", "2"],
 				},
 				{
 					id: "2",
@@ -34,6 +81,7 @@ class App extends React.Component {
 						x: 250,
 						y: 0,
 					},
+					outletPinNames: ["a", "b"],
 				},
 				{
 					id: "3",
@@ -41,6 +89,7 @@ class App extends React.Component {
 						x: 250,
 						y: 200,
 					},
+					outletPinNames: ["x", "y", "z"],
 				},
 			],
 			startNode: {
@@ -49,51 +98,53 @@ class App extends React.Component {
 					y: 300,
 				},
 			},
-			cable: {
-				position: {
-					x: 100,
-					y: 100,
+			cableDrag: {
+				isDragging: false,
+				initialPos: {
+					x: 0,
+					y: 0,
+				},
+				endPos: {
+					x: 0,
+					y: 0,
 				},
 			},
-			cableIsDragging: false,
-			initialCableDragPos: {
-				x: 100,
-				y: 100,
-			},
-			cableEndPos: {
-				x: 0,
-				y: 0,
-			},
 		};
+
+		console.info(stateSchema.validate(initialState));
+
+		this.state = initialState;
 
 		this.startDrag = this.startDrag.bind(this);
 		this.handlePatchSpaceClick = this.handlePatchSpaceClick.bind(this);
 		this.addNode = this.addNode.bind(this);
 		this.inspectNode = this.inspectNode.bind(this);
 		this.updateInspectedNodeId = this.updateInspectedNodeId.bind(this);
+		this.updateInspectedNodeOutletPinName = this.updateInspectedNodeOutletPinName.bind(
+			this
+		);
+		this.startCableDrag = this.startCableDrag.bind(this);
 		this.calcCableContainerPos = this.calcCableContainerPos.bind(this);
 		this.calcCableLine = this.calcCableLine.bind(this);
 	}
 
 	componentDidMount() {
 		window.addEventListener("mouseup", () =>
-			this.setState({ nodePinnedId: null, cableIsDragging: false })
+			this.setState((curState) =>
+				produce(curState, (draftState) => {
+					draftState.nodePinnedId = null;
+					draftState.cableDrag.isDragging = false;
+				})
+			)
 		);
 
 		window.addEventListener("mousemove", (e) => {
 			if (this.state.nodePinnedId !== null) {
 				const newPos = this.getMousePos(e);
-				//console.log(newPos);
-				// this.setState((curState) => ({
-				// 	nodePos: {
-				// 		x: newPos.x - curState.initialNodeDragOffset.x,
-				// 		y: newPos.y - curState.initialNodeDragOffset.y,
-				// 	},
-				// }));
+
 				if (this.state.nodePinnedId === "start") {
 					this.setState((curState) => ({
 						startNode: {
-							...curState.startNode,
 							position: {
 								x: newPos.x - curState.initialNodeDragOffset.x,
 								y: newPos.y - curState.initialNodeDragOffset.y,
@@ -101,42 +152,34 @@ class App extends React.Component {
 						},
 					}));
 				} else {
-					this.setState((curState) => ({
-						nodes: this.state.nodes.map((n) =>
-							n.id === curState.nodePinnedId
-								? {
-										...n,
-										nodePos: {
-											x:
-												newPos.x -
-												curState.initialNodeDragOffset
-													.x,
-											y:
-												newPos.y -
-												curState.initialNodeDragOffset
-													.y,
-										},
-								  }
-								: n
-						),
-					}));
+					this.setState((curState) =>
+						produce(curState, (draftState) => {
+							draftState.nodes.find(
+								(n) => n.id === draftState.nodePinnedId
+							).nodePos = {
+								x:
+									newPos.x -
+									draftState.initialNodeDragOffset.x,
+								y:
+									newPos.y -
+									draftState.initialNodeDragOffset.y,
+							};
+						})
+					);
 				}
 			}
 
-			if (this.state.cableIsDragging) {
+			if (this.state.cableDrag.isDragging) {
 				const mousePos = this.getMousePos(e);
-				this.setState((curState) => ({
-					cable: {
-						position: {
-							x: curState.initialCableDragPos.x,
-							y: curState.initialCableDragPos.y,
-						},
-					},
-					cableEndPos: {
-						x: mousePos.x,
-						y: mousePos.y,
-					},
-				}));
+
+				this.setState((curState) =>
+					produce(curState, (draftState) => {
+						draftState.cableDrag.endPos = {
+							x: mousePos.x,
+							y: mousePos.y,
+						};
+					})
+				);
 			}
 		});
 	}
@@ -170,23 +213,27 @@ class App extends React.Component {
 
 	addNode(e) {
 		e.preventDefault();
-		let newNodeId = 0;
+		let newNodeIdNum = 0;
 		while (
-			this.state.nodes.map((n) => n.id).includes(newNodeId.toString())
+			this.state.nodes.map((n) => n.id).includes("node-" + newNodeIdNum)
 		) {
-			newNodeId++;
+			newNodeIdNum++;
 		}
+		const newNodeId = "node-" + newNodeIdNum;
 		console.log(newNodeId, e);
-		this.setState((curState) => ({
-			nodeInspectedId: newNodeId.toString(),
-			nodes: curState.nodes.concat({
-				id: newNodeId.toString(),
-				nodePos: {
-					x: e.clientX,
-					y: e.clientY,
-				},
-			}),
-		}));
+		this.setState((curState) =>
+			produce(curState, (draftState) => {
+				draftState.nodeInspectedId = newNodeId;
+				draftState.nodes.push({
+					id: newNodeId,
+					nodePos: {
+						x: e.clientX,
+						y: e.clientY,
+					},
+					outletPinNames: ["success", "error"],
+				});
+			})
+		);
 	}
 
 	inspectNode(e, id) {
@@ -198,30 +245,76 @@ class App extends React.Component {
 	}
 
 	updateInspectedNodeId(e) {
-		this.setState((curState) => ({
-			nodeInspectedId: e.target.value,
-			nodes: this.state.nodes.map((n) =>
-				n.id === curState.nodeInspectedId
-					? {
-							...n,
-							id: e.target.value,
-					  }
-					: n
-			),
-		}));
+		this.setState((curState) =>
+			produce(curState, (draftState) => {
+				draftState.nodeInspectedId = e.target.value;
+				draftState.nodes.find(
+					(n) => n.id === curState.nodeInspectedId
+				).id = e.target.value;
+			})
+		);
 		console.log(e.target.value);
+	}
+
+	updateInspectedNodeOutletPinName(i, e) {
+		this.setState((curState) =>
+			produce(curState, (draftState) => {
+				draftState.nodes.find(
+					(n) => n.id === draftState.nodeInspectedId
+				).outletPinNames[i] = e.target.value;
+			})
+		);
+	}
+
+	deleteInspectedNodeOutletPin(i) {
+		// At least one outlet pin has to exist
+		if (
+			this.state.nodes.find((n) => n.id === this.state.nodeInspectedId)
+				.outletPinNames.length < 2
+		)
+			return;
+
+		this.setState((curState) =>
+			produce(curState, (draftState) => {
+				draftState.nodes
+					.find((n) => n.id === draftState.nodeInspectedId)
+					.outletPinNames.splice(i, 1);
+			})
+		);
+	}
+
+	addInspectedNodeOutletPin() {
+		this.setState((curState) =>
+			produce(curState, (draftState) => {
+				draftState.nodes
+					.find((n) => n.id === draftState.nodeInspectedId)
+					.outletPinNames.push("new outlet pin");
+			})
+		);
+	}
+
+	startCableDrag(e) {
+		e.stopPropagation();
+		console.log(e);
+		this.setState({
+			cableDrag: {
+				isDragging: true,
+				initialPos: {
+					x: e.clientX,
+					y: e.clientY,
+				},
+				endPos: {
+					x: e.clientX,
+					y: e.clientY,
+				},
+			},
+		});
 	}
 
 	calcCableContainerPos(startPos, endPos) {
 		return {
-			left:
-				endPos.x - startPos.x > 0
-					? this.state.cable.position.x
-					: endPos.x,
-			top:
-				endPos.y - startPos.y > 0
-					? this.state.cable.position.y
-					: endPos.y,
+			left: endPos.x - startPos.x > 0 ? startPos.x : endPos.x,
+			top: endPos.y - startPos.y > 0 ? startPos.y : endPos.y,
 		};
 	}
 
@@ -254,51 +347,54 @@ class App extends React.Component {
 					onContextMenu={(e) => this.addNode(e)}
 				>
 					{JSON.stringify(this.state)}
-					<svg
-						height={Math.abs(
-							this.state.cableEndPos.y -
-								this.state.initialCableDragPos.y
-						)}
-						width={Math.abs(
-							this.state.cableEndPos.x -
-								this.state.initialCableDragPos.x
-						)}
-						style={{
-							position: "absolute",
-							...this.calcCableContainerPos(
-								this.state.initialCableDragPos,
-								this.state.cableEndPos
-							),
-							// left: this.state.cable.position.x,
-							// top: this.state.cable.position.y,
-							backgroundColor: "rgb(0, 255, 0, 0.1)",
-						}}
-					>
-						<line
-							x1={this.calcCableLine(
-								"x1",
-								this.state.initialCableDragPos,
-								this.state.cableEndPos
+					{this.state.cableDrag.isDragging ? (
+						<svg
+							height={Math.abs(
+								this.state.cableDrag.endPos.y -
+									this.state.cableDrag.initialPos.y
 							)}
-							y1={this.calcCableLine(
-								"y1",
-								this.state.initialCableDragPos,
-								this.state.cableEndPos
+							width={Math.abs(
+								this.state.cableDrag.endPos.x -
+									this.state.cableDrag.initialPos.x
 							)}
-							x2={this.calcCableLine(
-								"x2",
-								this.state.initialCableDragPos,
-								this.state.cableEndPos
-							)}
-							y2={this.calcCableLine(
-								"y2",
-								this.state.initialCableDragPos,
-								this.state.cableEndPos
-							)}
-							style={{ stroke: "rgb(255,0,0)", strokeWidth: "2" }}
-						/>
-						Sorry, your browser does not support inline SVG.
-					</svg>
+							style={{
+								position: "absolute",
+								...this.calcCableContainerPos(
+									this.state.cableDrag.initialPos,
+									this.state.cableDrag.endPos
+								),
+								backgroundColor: "rgb(0, 255, 0, 0.1)",
+							}}
+						>
+							<line
+								x1={this.calcCableLine(
+									"x1",
+									this.state.cableDrag.initialPos,
+									this.state.cableDrag.endPos
+								)}
+								y1={this.calcCableLine(
+									"y1",
+									this.state.cableDrag.initialPos,
+									this.state.cableDrag.endPos
+								)}
+								x2={this.calcCableLine(
+									"x2",
+									this.state.cableDrag.initialPos,
+									this.state.cableDrag.endPos
+								)}
+								y2={this.calcCableLine(
+									"y2",
+									this.state.cableDrag.initialPos,
+									this.state.cableDrag.endPos
+								)}
+								style={{
+									stroke: "rgb(255,0,0)",
+									strokeWidth: "2",
+								}}
+							/>
+							Sorry, your browser does not support inline SVG.
+						</svg>
+					) : null}
 					{this.state.nodes.map((n) => (
 						<div
 							id={n.id}
@@ -318,29 +414,23 @@ class App extends React.Component {
 							<div className="body"></div>
 							<div
 								className="node-box__inlet-pin"
-								onMouseDown={(e) => {
-									e.stopPropagation();
-									console.log(e);
-									this.setState({
-										cableIsDragging: true,
-										initialCableDragPos: {
-											x: e.clientX,
-											y: e.clientY,
-										},
-										cableEndPos: {
-											x: e.clientX,
-											y: e.clientY,
-										},
-										// cable: {
-										// 	position: {
-										// 		x: e.clientX,
-										// 		y: e.clientY
-										// 	}
-										// }
-									});
-								}}
+								onMouseDown={(e) => this.startCableDrag(e)}
 							></div>
-							<div className="node-box__outlet-pin"></div>
+							{n.outletPinNames.map((pinName, i) => (
+								<div
+									className="node-box__outlet-pin"
+									style={{
+										top:
+											(i + 1) *
+												(150 /
+													(n.outletPinNames.length +
+														1)) -
+											5 +
+											"px",
+									}}
+									title={pinName}
+								></div>
+							))}
 						</div>
 					))}
 					<div
@@ -376,14 +466,64 @@ class App extends React.Component {
 				</div>
 				<div className="sidebar-inspector">
 					{this.state.nodeInspectedId !== null ? (
-						<div className="input-group">
-							<div className="input-label">Id:</div>
-							<input
-								type="text"
-								className="input-field"
-								value={this.state.nodeInspectedId}
-								onChange={(e) => this.updateInspectedNodeId(e)}
-							/>
+						<div>
+							<div className="settings-group">
+								<div className="settings-group__id-input-label">
+									Id:
+								</div>
+								<input
+									type="text"
+									className="settings-group__id-input-field"
+									value={this.state.nodeInspectedId}
+									onChange={(e) =>
+										this.updateInspectedNodeId(e)
+									}
+								/>
+							</div>
+							<hr style={{ margin: "0" }} />
+							<div className="settings-group">
+								<div className="settings-group__outlet-pins-label">
+									Outlet pin names:
+								</div>
+								{this.state.nodes
+									.find(
+										(n) =>
+											n.id === this.state.nodeInspectedId
+									)
+									.outletPinNames.map((pinName, i) => (
+										<div>
+											<input
+												type="text"
+												className="settings-group__outlet-pin-name-input-field"
+												value={pinName}
+												onChange={(e) =>
+													this.updateInspectedNodeOutletPinName(
+														i,
+														e
+													)
+												}
+											/>
+											<button
+												className="settings-group__outlet-pin-delete-button"
+												onClick={() =>
+													this.deleteInspectedNodeOutletPin(
+														i
+													)
+												}
+											>
+												Delete
+											</button>
+										</div>
+									))}
+								<button
+									className="settings-group__outlet-pin-add-button"
+									onClick={() =>
+										this.addInspectedNodeOutletPin()
+									}
+								>
+									Add pin
+								</button>
+							</div>
 						</div>
 					) : null}
 				</div>
